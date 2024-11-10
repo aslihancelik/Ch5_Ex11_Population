faac1fcea2dbfbeb0c5038b5da0452b8c3e3bf77
Aslihan Aria Celik <aslihancelik27@gmail.com>
2024-11-09 20:51:16 -0500

    done

--- Ch5_Ex11_Population.cpp
+++ Ch5_Ex11_Population.cpp
@@ -1,11 +1,15 @@
 // Ch5_Ex11_Population.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+// This program calculates the new population size after the growth which its rate is specified by the user.
+// The program outputs the population sizes after growth for the amount of days requested by the user.
+//
+//Programmer: Aslihan Aria Celik
 
 
 #include <iostream>
 using namespace std;
 
+//the function for calculating the new population after growth
 int calculatePopulation(int startSize, int increase) {
   
     return startSize + (startSize * increase / 100);
@@ -17,6 +21,8 @@ int main()
     int increasePercentage; 
     int daysToMultiply;
 
+    //prompting the user to enter the starting number of organisms
+    //validates the input by making sure it is not smaller than 2
     do {
         cout << "What is the starting number of organisms?" << endl;
         cin >> startingSize;
@@ -26,6 +32,9 @@ int main()
         }
     } while (startingSize < 2);
     
+    //prompting the user to enter the percentaghe of the population increase
+    //validates the input by making sure it is not a negative number
+
     do {
         cout << "What is their average daily population increase?" << endl;
         cout << "Please enter the value of the percentage." << endl;
@@ -36,6 +45,9 @@ int main()
         }
     } while (increasePercentage < 0);
 
+    //prompting the user to enter the number of days they request the calcualtion for
+    //validates the input by making sure it is not less than 1
+
     do {
         cout << "What is the number of days you would like to see the population size with the growth?" << endl;
         cin >> daysToMultiply;
@@ -45,9 +57,12 @@ int main()
         }
     } while (daysToMultiply < 1);
 
+    //holding the starting size of the population in a temp variable for calculation purposes
     int newPopulation = startingSize;
     cout << "The population size on day 1, which is the starting size is: " << startingSize << endl;
+    //does the calculation for the specified number of days by the user
     for (int i = 1; i <= daysToMultiply; ++i) {
+        //temprorarily holds the calcualted population size in the newPopulation variable and updates it each iteration
         newPopulation = calculatePopulation(newPopulation, increasePercentage);
         cout << "The population size on day " << i + 1 << " is: " << newPopulation << endl;
     }
